d5cdf01c6416275c490550c06a4ff58890d0e7d5
shinde_sushil <sushil@brandmovers.in>
2019-03-24 21:23:41 +0530

    moved templates

--- firstapp/webapp/views.py
+++ firstapp/webapp/views.py
@@ -5,11 +5,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def about(request):
     context = {
         'title' : 'First app',
         'content' : 'This is content of app'
     }
     return render(request, 'index.html', context=context)
+
+def about(request):
+    return render(request, 'about.html')
